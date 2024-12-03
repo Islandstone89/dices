5dc07d931a1b02fe02a6e0ad478ac016ac98992a
Øystein Håberg <islandstone89@gmail.com>
2024-12-03 23:10:23 +0100

    * Ended up using a simple function that toggles a "dark" class on the body, and using :has change styles depending on the class is on or off

--- script.js
+++ script.js
@@ -1,47 +1,9 @@
 
-/* SELECTORS */
+let body = document.querySelector("body")
+let themeToggle = document.querySelector(".js-btn");
 
-let themeToggle = document.querySelector(".js-btn1");
-let themeToggle2 = document.querySelector(".js-btn2");
-
-/* themeToggle.addEventListener("click", () => {
-    if (themeToggle.getAttribute("data-theme" ,"light")) {
-        themeToggle.setAttribute("data-theme", "dark")
-        console.log("success");
-    }
-
-    else if (themeToggle.getAttribute("data-theme" ,"dark")) {
-        themeToggle.setAttribute("data-theme", "light")
-        console.log("success again");
-    }
-}) */
-
-themeToggle.addEventListener("click", () => {
-    themeToggle.setAttribute("data-theme", "dark")
-})
-
-
-themeToggle2.addEventListener("click", () => {
-    themeToggle2.setAttribute("data-theme", "light")
-})
-
-
-
-
-
-/* 
-function changeTheme () {
-    if (themeToggle.getAttribute("data-theme" ,"light")) {
-        themeToggle.setAttribute("data-theme", "dark")
-        console.log("success");
-    }
-
-    else {
-        themeToggle.setAttribute("data-theme", "light")
-        console.log("success again");
-    }
+function switchTheme() {
+    body.classList.toggle("dark");
 }
 
-
-
-themeToggle.addEventListener("click", changeTheme); */
+themeToggle.addEventListener("click", switchTheme);
